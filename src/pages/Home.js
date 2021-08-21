d60f8cb278d75afd52297a8bfaa4b51e7f398ac0
faisal-kn <khanmohdfaisal75@gmail.com>
2021-08-21 20:40:11 +0530

    created home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./Home.module.css";
 
 const home = () => {
-    return (
-        <>
-        <p>Welcome here</p>
-        </>
-    )
+  return (
+    <div className={classes.container}>
+      <NavLink to="/login">Log in</NavLink>
+      <NavLink to="/signup">Sign up</NavLink>
+      <div className={classes.cont}>
+        <p>Welcome to our url-shortner</p>
+        <p className={classes.middle}>
+          Please login to the website to create your link
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default home;
